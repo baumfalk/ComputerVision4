b306b7388da904022f14d3e88cd6b9da7e4dfeef
Jetze Baumfalk <jetze@baumfalk.nl>
2014-02-06 17:00:36 +0100

    shizzle works a little bit

--- include/Detector.h
+++ include/Detector.h
@@ -1,9 +1,10 @@
 /*
- * Detector.h
- *
- *  Created on: Aug 23, 2013
- *      Author: coert
- */
+* Detector.h
+*
+*  Created on: Aug 23, 2013
+*      Author: coert
+*/
+
 
 #ifndef DETECTOR_H_
 #define DETECTOR_H_
@@ -19,64 +20,64 @@
 namespace nl_uu_science_gmt
 {
 
-class Detector
-{
-	static QueryXML* _Config;
-	static std::string ConfigPath;
-
-	const int _pos_amount;
-	const int _target_width;
-	const int _posneg_factor; // 4x as many negative images as positive ones
-	const int _seed;
-	const int _disp;
-	const size_t _max_count;
-	const double _epsilon;
-	const int _max_image_size;
-	const int _layer_scale_interval;
-	const int _initial_threshold;
-	const double _overlap_threshold;
-	const bool _do_equalizing;
-	const bool _do_whitening;
-	const double _gt_accuracy;
-	const std::string _query_image_file;
-
-	cv::Size _model_size;
-	cv::Mat _neg_sum8U, _pos_sum8U, _pos_sumF;
-	double _layer_scale_factor;
-
-	void readPosFilelist(std::vector<std::string> &);
-	void readNegFilelist(std::vector<std::string> &);
-
-	void pixelsToHOG(cv::Mat &, cv::Mat &);
-	void readPosData(const std::vector<std::string> &, cv::Mat &);
-	void readNegData(const std::vector<std::string> &, cv::Mat &);
-
-	void createPyramid(const cv::Mat &, std::vector<cv::Mat*> &);
-
-public:
-	Detector(const std::string &);
-	virtual ~Detector();
-
-	void run();
-
-	static std::string StorageExt;
-	static std::string ImageExt;
-
-	static QueryXML*
-	cfg()
+	class Detector
 	{
-		if (!_Config) // Only allow one instance of class to be generated.
-			_Config = new QueryXML(ConfigPath);
+		static QueryXML* _Config;
+		static std::string ConfigPath;
 
-		return _Config;
-	}
+		const int _pos_amount;
+		const int _target_width;
+		const int _posneg_factor; // 4x as many negative images as positive ones
+		const int _seed;
+		const int _disp;
+		const size_t _max_count;
+		const double _epsilon;
+		const int _max_image_size;
+		const int _layer_scale_interval;
+		const int _initial_threshold;
+		const double _overlap_threshold;
+		bool _do_equalizing;
+		bool _do_whitening;
+		const double _gt_accuracy;
+		const std::string _query_image_file;
+		std::vector<std::vector<float>> _descriptors;
 
-	static void cfg_destroy()
-	{
-		if (_Config) delete _Config;
-		_Config = NULL;
-	}
-};
+		cv::Size _model_size;
+		cv::Mat _neg_sum8U, _pos_sum8U, _pos_sumF;
+		double _layer_scale_factor;
+
+		void readPosFilelist(std::vector<std::string> &);
+		void readNegFilelist(std::vector<std::string> &);
+
+		void readPosData(const std::vector<std::string> &, cv::Mat &, cv::Mat &);
+		void readNegData(const std::vector<std::string> &, cv::Mat &, cv::Mat &);
+
+		void createPyramid(const cv::Mat &, std::vector<cv::Mat*> &);
+
+	public:
+		Detector(const std::string &);
+		virtual ~Detector();
+
+		void run();
+
+		static std::string StorageExt;
+		static std::string ImageExt;
+
+		static QueryXML*
+			cfg()
+		{
+				if (!_Config) // Only allow one instance of class to be generated.
+					_Config = new QueryXML(ConfigPath);
+
+				return _Config;
+			}
+
+		static void cfg_destroy()
+		{
+			if (_Config) delete _Config;
+			_Config = NULL;
+		}
+	};
 
 } /* namespace nl_uu_science_gmt */
 #endif /* DETECTOR_H_ */
